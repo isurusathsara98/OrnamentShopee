d0d58803659a907f673cd009841d5b9469d8bb24
Isuru Sathsara <Isuru.Sa@CreativeSoftware.com>
2024-03-25 23:46:17 +0530

    Cool nav bar added

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
+using MatBlazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using OramentShopee;
@@ -5,7 +6,7 @@ using OramentShopee;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddMatBlazor();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
